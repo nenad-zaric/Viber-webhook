751e6c0106c6f32fb811e04640f02ef1e2911152
nenad <nenadzaric.shone@gmail.com>
2023-05-18 15:06:23 +0200

    added delete subscriber message

--- app.py
+++ app.py
@@ -33,6 +33,9 @@ def webhook():
     if data['event'] == 'subscribed':
         create_subscriber(data)
 
+    elif data['event'] == 'unsubscribed':
+        delete_subscriber(data)
+
     return '', 200
 
 @app.route('/subscribers', methods=['GET'])
@@ -58,31 +61,29 @@ def get_subscribers():
 def create_subscriber(subscriber_data):
     if subscriber_data['event'] == 'subscribed':
         viber_id = subscriber_data['user']['id']
-        try:
-            message_text = get_subscriber_message(viber_id)
+        message_text = get_subscriber_message(viber_id)
 
-            subscriber = Subscriber(
-                viber_id=viber_id,
-                name=subscriber_data['user']['name'],
-                avatar=subscriber_data['user']['avatar'],
-                country=subscriber_data['user']['country'],
-                language=subscriber_data['user']['language'],
-                api_version=subscriber_data['user']['api_version'],
-                member_id=message_text
-            )
-        except Exception:
-            subscriber = Subscriber(
-                viber_id=viber_id,
-                name=subscriber_data['user']['name'],
-                avatar=subscriber_data['user']['avatar'],
-                country=subscriber_data['user']['country'],
-                language=subscriber_data['user']['language'],
-                api_version=subscriber_data['user']['api_version'],
-                member_id="1"
-            )
+        subscriber = Subscriber(
+            viber_id=viber_id,
+            name=subscriber_data['user']['name'],
+            avatar=subscriber_data['user']['avatar'],
+            country=subscriber_data['user']['country'],
+            language=subscriber_data['user']['language'],
+            api_version=subscriber_data['user']['api_version'],
+            member_id=message_text
+        )
+
 
         db.session.add(subscriber)
         db.session.commit()
+
+def delete_subscriber(data):
+    if data['event'] == 'unsubscribed':
+        viber_id = data['user_id']
+        subscriber = Subscriber.query.filter_by(viber_id=viber_id).first()
+        if subscriber:
+            db.session.delete(subscriber)
+            db.session.commit()
 
 def get_subscriber_message(viber_id):
     subscriber_message = SubscriberMessage.query.filter_by(viber_id=viber_id).first()
@@ -91,8 +92,14 @@ def get_subscriber_message(viber_id):
     return ''
 
 def store_subscriber_message(viber_id, message_text):
-    subscriber_message = SubscriberMessage(viber_id=viber_id, message=message_text)
-    db.session.add(subscriber_message)
+    subscriber = Subscriber.query.filter_by(viber_id=viber_id).first()
+    subscriber_message = SubscriberMessage.query.filter_by(viber_id=viber_id).first()
+
+    if subscriber:
+        db.session.delete(subscriber)
+    if subscriber_message:
+        db.session.delete(subscriber_message)
+    
     db.session.commit()
 
 if __name__ == '__main__':
