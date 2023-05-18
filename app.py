5a992edbddd9b712ad854cb52b11b5e48392cb3f
nenad <nenadzaric.shone@gmail.com>
2023-05-18 12:21:03 +0200

    changed to postgres

--- app.py
+++ app.py
@@ -1,89 +1,87 @@
-from flask import Flask, request
-import sqlite3
+from flask import Flask, request, jsonify
+from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
-DATABASE = 'subscribers.db'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'postgres://<username>:<password>@<host>:<port>/<database_name>'
+db = SQLAlchemy(app)
 
-def create_subscriber(subscriber_data):
-    conn = sqlite3.connect(DATABASE)
-    cursor = conn.cursor()
+class Subscriber(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    viber_id = db.Column(db.Text, nullable=False)
+    name = db.Column(db.Text)
+    avatar = db.Column(db.Text)
+    country = db.Column(db.Text)
+    language = db.Column(db.Text)
+    api_version = db.Column(db.Integer)
+    member_id = db.Column(db.Text)
 
-    # Check if the event type is 'subscribed'
-    if subscriber_data['event'] == 'subscribed':
-        viber_id = subscriber_data['user']['id']
-        message_text = get_subscriber_message(viber_id)
-
-        # Store the subscriber's data and the content of the first message in the member_id field
-        cursor.execute("""
-            INSERT INTO subscribers (viber_id, name, avatar, country, language, api_version, member_id)
-            VALUES (?, ?, ?, ?, ?, ?, ?)
-        """, (
-            viber_id,
-            subscriber_data['user']['name'],
-            subscriber_data['user']['avatar'],
-            subscriber_data['user']['country'],
-            subscriber_data['user']['language'],
-            subscriber_data['user']['api_version'],
-            message_text
-        ))
-        conn.commit()
-
-    conn.close()
-
-def get_subscriber_message(viber_id):
-    conn = sqlite3.connect(DATABASE)
-    cursor = conn.cursor()
-
-    # Retrieve the stored message for the subscriber
-    cursor.execute("SELECT message FROM subscriber_messages WHERE viber_id = ?", (viber_id,))
-    result = cursor.fetchone()
-
-    conn.close()
-
-    if result is not None:
-        return result[0]
-    else:
-        return ''
-
-def store_subscriber_message(viber_id, message_text):
-    conn = sqlite3.connect(DATABASE)
-    cursor = conn.cursor()
-
-    # Store the message for the subscriber
-    cursor.execute("INSERT INTO subscriber_messages (viber_id, message) VALUES (?, ?)", (viber_id, message_text))
-    conn.commit()
-
-    conn.close()
+class SubscriberMessage(db.Model):
+    viber_id = db.Column(db.Text, primary_key=True)
+    message = db.Column(db.Text)
 
 @app.route('/webhook', methods=['POST'])
 def webhook():
     data = request.json
 
-    # Check if it's a message event and store the message content for the subscriber
     if data['event'] == 'message':
         viber_id = data['sender']['id']
         message_text = data['message'].get('text', '')
 
-        # Store the first message for the subscriber if not already stored
         if get_subscriber_message(viber_id) == '':
             store_subscriber_message(viber_id, message_text)
 
-    # Handle the subscribed event
     if data['event'] == 'subscribed':
         create_subscriber(data)
 
     return '', 200
 
-if __name__ == '__main__':
-    # Create the subscriber_messages table if it doesn't exist
-    conn = sqlite3.connect(DATABASE)
-    cursor = conn.cursor()
-    cursor.execute("""
-        CREATE TABLE IF NOT EXISTS subscriber_messages (
-            viber_id TEXT PRIMARY KEY,
-            message TEXT
-        )
-    """)
-    conn.close()
+@app.route('/subscribers', methods=['GET'])
+def get_subscribers():
+    subscribers = Subscriber.query.all()
+    subscriber_list = []
 
+    for subscriber in subscribers:
+        subscriber_data = {
+            'id': subscriber.id,
+            'viber_id': subscriber.viber_id,
+            'name': subscriber.name,
+            'avatar': subscriber.avatar,
+            'country': subscriber.country,
+            'language': subscriber.language,
+            'api_version': subscriber.api_version,
+            'member_id': subscriber.member_id
+        }
+        subscriber_list.append(subscriber_data)
+
+    return jsonify(subscriber_list)
+
+def create_subscriber(subscriber_data):
+    if subscriber_data['event'] == 'subscribed':
+        viber_id = subscriber_data['user']['id']
+        message_text = get_subscriber_message(viber_id)
+
+        subscriber = Subscriber(
+            viber_id=viber_id,
+            name=subscriber_data['user']['name'],
+            avatar=subscriber_data['user']['avatar'],
+            country=subscriber_data['user']['country'],
+            language=subscriber_data['user']['language'],
+            api_version=subscriber_data['user']['api_version'],
+            member_id=message_text
+        )
+        db.session.add(subscriber)
+        db.session.commit()
+
+def get_subscriber_message(viber_id):
+    subscriber_message = SubscriberMessage.query.filter_by(viber_id=viber_id).first()
+    if subscriber_message:
+        return subscriber_message.message
+    return ''
+
+def store_subscriber_message(viber_id, message_text):
+    subscriber_message = SubscriberMessage(viber_id=viber_id, message=message_text)
+    db.session.add(subscriber_message)
+    db.session.commit()
+
+if __name__ == '__main__':
     app.run()
